91f89a65148417b36e6d9293839ceb338ff5f530
paramirr <paramirr@gmail.com>
2014-03-17 23:30:59 +0900

    1.16 solved

---
+++ chapter_1_2/exercise_1_16.rkt
@@ -0,0 +1,16 @@
+(module exercise_1_16 racket
+    (define (fast-expt-iter b n)
+        (define (even? x)
+            (= (remainder x 2) 0))
+        (define (iter a n)
+            (cond ((= n 0) a)
+            ((even? n) (displayln a) (displayln n) (iter (* a a) (/ n 2)))
+            (else (displayln a) (displayln n) (iter (* a b) (- n 1)))))
+        (if (= n 0) 1
+            (iter b (- n 1)))
+    )
+
+    (fast-expt-iter 3 20)
+    (fast-expt-iter 2 10)
+
+)
